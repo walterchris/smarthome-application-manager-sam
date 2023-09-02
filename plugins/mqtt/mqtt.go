9ab62adfbb9ec067314e18987edc85f006b80bf0
Christian Walter <christian.walter@9elements.com>
2023-09-02 21:01:56 +0200

    feat(treewide): add struct for sending mqtt msgs
    Signed-off-by: Christian Walter <christian.walter@9elements.com>

--- plugins/mqtt/mqtt.go
+++ plugins/mqtt/mqtt.go
@@ -1,17 +1,30 @@
 package mqtt
 
-import "github.com/walterchris/smarthome-application-manager-sam/pkg/communication"
+import (
+	"github.com/sirupsen/logrus"
+	"github.com/walterchris/smarthome-application-manager-sam/pkg/communication"
+)
+
+var log *logrus.Logger
 
 type MQTT struct {
 	Channels communication.Channels
 }
 
-func New() (*MQTT, error) {
+func New(logger *logrus.Logger) (*MQTT, error) {
+	log = logger
+
 	return &MQTT{
-		Channels: communication.Channels{},
+		Channels: communication.Channels{
+			Messages: make(chan communication.Mqtt),
+		},
 	}, nil
 }
 
 func (mqtt *MQTT) Run() {
-	// Set up MQTT, populate topics (How do know what plugins need what topics?)
+	// We only need a client to post to our MQTT broker
+	for {
+		mqttMsg := <-mqtt.Channels.Messages
+		log.Printf("MQTT Message '%s' on '%s'\n", mqttMsg.Msg, mqttMsg.Topic)
+	}
 }
